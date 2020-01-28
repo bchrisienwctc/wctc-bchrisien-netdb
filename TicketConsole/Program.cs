cb2801e90bf1ae563ddcae52b3f5d072e9d665ac
BIT student <bitstudent@bitnet.wctc.edu>
2020-01-27 20:47:09 -0600

    Add project files.

---
+++ TicketConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TicketConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many people are watching this ticket?");
+            int watchingNum = 3;
+            string Watching = "";
+            for(int i=1;i<=watchingNum;i++)
+            {
+                Console.WriteLine("What is the name of Watcher " + i + "?");
+                Watching += Console.ReadLine();
+
+                if (i == watchingNum)
+                {
+                    break;
+                }
+                Watching += "|";
+
+            }
+            Console.WriteLine(Watching);
+        }
+    }
+}
---
+++ lesson2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bogus = new string[]
+           {
+                "tom",
+                "richard",
+                "harry"
+           };
+            Console.WriteLine($"The Second name is: {bogus[1]}");
+            for (int i = 0; i < bogus.length; i++)
+            {
+
+            }
+
+            string myString = "";
+            var myString2;
+
+            foreach(string name in bogus)
+            {
+                Console.WriteLine($"The second name is: {name}");
+            }
+        }
+    }
+}
